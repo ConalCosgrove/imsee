e140843dcfab8303266128e7b49185c8b529bf5c
Conal Cosgrove <conal.cosgrove@salesforce.com>
2019-07-07 00:09:45 +0100

    Added table view for rooms

--- src/components/Inspector/RoomInspector.jsx
+++ src/components/Inspector/RoomInspector.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import './Inspector.css';
+
+const Row = ({name, value}) => {
+  return (
+  <tr className="Row">
+    <td className="Name">{name}</td>
+    <td className="Value">{JSON.stringify(value)}</td>
+    <td><input></input></td>
+  </tr>
+  );
+}
+
+const Table = ({data}) => {
+  return (
+  <table className="Table">
+    <tr className="Row">
+      <th>Parameter</th>
+      <th>Value</th>
+      <th>Update</th>
+    </tr>
+    {
+      Object.keys(data).map((key) => {
+        return <Row name={key} value={data[key]}/>
+      })
+    }
+  </table>
+  );
+}
+
+const buildTable = (data) => {
+  return <Table data={data}/>;
+}
 
 const RoomInspector = ({data}) => {
-  return <div><h1>Room: {data.name}</h1></div>
+  return (
+  <div>
+    <h1>Room: {data.name}</h1>
+    {buildTable(data)}
+  </div>
+  );
 }
+
+
 
 export default RoomInspector;
